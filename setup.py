ac2b74cd96c563cd34760d6aabf61b8cb9fe258d
zhurong2020 <zhurong0525@gmail.com>
2025-02-26 17:28:24 -0500

    fix: version0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lipid-risk-assessor",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         'tkinter',  # 通常内置于Python
