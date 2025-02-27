840fa75a8d382030601414e064973bacdc407c7e
zhurong2020 <zhurong0525@gmail.com>
2025-02-26 19:27:49 -0500

    fix: add execute right to deploy_hyperlipidemia.sh

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lipid-risk-assessor",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         'tkinter',  # 通常内置于Python
